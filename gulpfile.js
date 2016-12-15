87040c57442bfcce6e11e7925dc2979910c70062
wk633 <wk633@outlook.com>
2016-12-15 14:06:17 +0800

    调整了gulp的监视目录

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ gulp.task('server', function(){
   server.run(['app.js']);
 
   // restart the server when app.js changes
-  gulp.watch(['app.js', 'public/js/*.js','schemas/*.js','models/*.js'], server.run)
-  gulp.watch(['views/**/*.jade'], server.notify);
+  gulp.watch(['app.js', 'public/js/*.js','app/**/*.js', 'config/routes.js'], server.run)
+  gulp.watch(['app/views/**/*.jade'], server.notify);
 })
 
 gulp.task('default',['server'])
