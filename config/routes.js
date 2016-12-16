5fb7bc607367932749b6788c4a826d8d8c2582c7
wk633 <wk633@outlook.com>
2016-12-16 09:26:03 +0800

    增加/movie/list post请求的admin要求

--- config/routes.js
+++ config/routes.js
@@ -27,8 +27,11 @@ module.exports = function(app){
   app.get('/detail/:id', Movie.detail)
   app.get('/admin/movie/new',User.signinRequired, User.adminRequired, Movie.new) // 录入界面
   app.get('/admin/movie/update/:id', User.signinRequired, User.adminRequired ,Movie.update)
-  app.post('/admin/movie', Movie.record) // 录入提交
+  app.post('/admin/movie',User.signinRequired, User.adminRequired, Movie.record) // 录入提交
   app.get('/admin/list', User.signinRequired, User.adminRequired, Movie.list) // 查看用户
   app.delete('/admin/movie/delete', Movie.delete)
+
+  // Comment
+
 
 }
